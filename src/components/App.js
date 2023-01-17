234adebefcf0f31cab57410efbc01838474a7cd9
ShinJaeIl01 <ekvm555@naver.com>
2023-01-17 12:25:56 +0900

    feat: Checking Firebase Current User

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,15 @@
-import React from "react";
-import AppRouter from "./Router";
+import React, { useState } from "react";
+import AppRouter from "components/Router";
+import { authService } from "fbase";
 
 const App = () => {
-  return <AppRouter />;
+  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
+  return (
+    <>
+      <AppRouter isLoggedIn={isLoggedIn} />
+      <footer>&copy; {new Date().getFullYear()} Jwitter</footer>
+    </>
+  );
 };
 
 export default App;

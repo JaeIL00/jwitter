560dcd3494fe9ca861e8ef5bd05fa6eaa8b0b5fb
ShinJaeIl01 <ekvm555@naver.com>
2023-01-18 17:06:11 +0900

    feat: Create Realtime Jweets

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,7 +1,71 @@
-import React from "react";
+import { dbService } from "fbase";
+import {
+  addDoc,
+  collection,
+  onSnapshot,
+  orderBy,
+  query,
+} from "firebase/firestore";
+import React, { useEffect, useState } from "react";
 
-const Home = () => {
-  return <div>Home</div>;
+const Home = ({ userObj }) => {
+  const [jweet, setJweet] = useState("");
+  const [jweets, setJweets] = useState([]);
+
+  useEffect(() => {
+    const dbJweets = query(
+      collection(dbService, "jweets"),
+      orderBy("createdAt", "desc")
+    );
+    onSnapshot(dbJweets, (snapshot) => {
+      const jweetArr = snapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+      setJweets(jweetArr);
+    });
+  }, []);
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      await addDoc(collection(dbService, "jweets"), {
+        text: jweet,
+        createdAt: Date.now(),
+        creatorId: userObj.uid,
+      });
+      setJweet("");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setJweet(value);
+  };
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          placeholder="What's on your mind?"
+          maxLength={120}
+          value={jweet}
+          onChange={onChange}
+        />
+        <input type="submit" value="Jweet" />
+      </form>
+      <div>
+        {jweets.map(({ id, text }) => (
+          <div key={id}>
+            <h4>{text}</h4>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Home;

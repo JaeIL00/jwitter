4c2ba143550a99fb23aff47ace0faf1c000bc5bd
ShinJaeIl01 <ekvm555@naver.com>
2023-01-20 15:46:59 +0900

    Reset Style

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App";
+import { Reset } from "styled-reset";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <>
+    <Reset />
+    <App />
+  </>
+);
